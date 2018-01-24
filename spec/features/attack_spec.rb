31dbd2de57cdd5098c104c0b53d5fcdf1f6e22c3
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-24 17:12:26 +0000

    repeated the entire exercise from scratch.

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,6 +1,5 @@
-feature 'Attack' do
-  scenario 'player 1 attacks player 2' do
-    # sign_in_and_play
+feature 'Attack the opponent player' do
+  scenario 'Player 1 attacks player 2 and sees confirmation' do
     session = sign_in_session_and_play
     session.click_link 'Attack'
     expect(session).to have_content 'Yuliya attacks Alan'
