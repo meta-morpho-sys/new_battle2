52261291e9236f2c36c3ec065dbcaecf8d7c8d51
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-25 23:24:17 +0000

    Added feature of accessing instances of Players through Game.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,17 @@
 # Manages attacks and other rules.
 class Game
+  def initialize(player1, player2)
+    @players = [player1, player2]
+  end
+
+  def player1
+    @players.first
+  end
+
+  def player2
+    @players.last
+  end
+
   def attack(player)
     player.receive_damage
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,21 @@
 require 'game'
 
 describe Game do
-  subject(:game) { Game.new }
+  subject(:game) { Game.new(alan, yuliya) }
   let(:alan) { double :player }
   let(:yuliya) { double :player }
+
+  describe '#player1' do
+    it 'retrieves the first player' do
+      expect(game.player1).to eq alan
+    end
+  end
+
+  describe '#player2' do
+    it 'retrieves the second player' do
+      expect(game.player2).to eq yuliya
+    end
+  end
 
   describe '#attack' do
     it 'damages the player' do
