a2a2c0cad34d4f61031641a52f217d0a23d4db13
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-27 18:05:24 +0000

    Unit tests and implememntations for end-of-game feature.

--- lib/game.rb
+++ lib/game.rb
@@ -15,10 +15,6 @@ class Game
     @players.last
   end
 
-  # def attack(player)
-  #   player.receive_damage
-  # end
-
   def attack(player = opponent_of(current_turn))
     player.receive_damage
   end
@@ -29,5 +25,19 @@ class Game
 
   def opponent_of(the_player)
     @players.reject { |player| player == the_player }.first
+  end
+
+  def game_over?
+    losing_players.any?
+  end
+
+  def loser
+    losing_players.first
+  end
+
+  private
+
+  def losing_players
+    @players.select { |player| player.hit_points <= 0 }
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,10 @@ require 'game'
 
 describe Game do
   subject(:game) { Game.new(player1, player2) }
-  let(:player1) { double :player1, name: 'Alan' }
-  let(:player2) { double :player2, name: 'Yuliya' }
+  subject(:finished_game) { Game.new(dead_player, player2) }
+  let(:player1) { double :player, hit_points: 60 }
+  let(:player2) { double :player, hit_points: 60 }
+  let(:dead_player) { double :player, hit_points: 0 }
 
   describe '#player1' do
     it 'retrieves the first player' do
@@ -41,6 +43,22 @@ describe Game do
     it 'finds the opponent of the player' do
       expect(game.opponent_of(player1)).to eq player2
       expect(game.opponent_of(player2)).to eq player1
+    end
+  end
+
+  describe '#game_over?' do
+    it 'returns false if no-one of the players is at 0 HP' do
+      expect(game.game_over?).to eq false
+    end
+
+    it 'returns true if one of the players is at 0 HP' do
+      expect(finished_game.game_over?).to eq true
+    end
+  end
+
+  describe '#loser' do
+    it 'returns the player on less than 0 HP' do
+      expect(finished_game.loser).to eq dead_player
     end
   end
 end
