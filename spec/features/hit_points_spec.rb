9946ab9c0e2a859a70e527a8eeb63363fd0669cb
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-02-08 15:18:44 +0000

    refactoring before adding on a new feature.

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,29 +1,27 @@
 feature 'Displaying hit points' do
-  scenario 'see hit points for Player 1' do
-    # session = sign_in_session_and_play
-    # expect(session).to have_content 'Alan: 60 HP'
-    sign_in_and_play
-    expect(page).to have_content 'Yuliya: 60 HP'
+  context 'at the beginning of the game' do
+    before { sign_in_and_play }
+    scenario 'see hit points for Player 1' do
+      expect(page).to have_content 'Yuliya: 60 HP'
+    end
+
+    scenario 'see hit points for Player 2' do
+      expect(page).to have_content 'Alan: 60 HP'
+    end
   end
 
-  scenario 'see hit points for Player 2' do
-    # session = sign_in_session_and_play
-    # expect(session).to have_content 'Alan: 60 HP'
-    sign_in_and_play
-    expect(page).to have_content 'Alan: 60 HP'
-  end
+  context 'while playing the game' do
+    before { sign_in_and_play; attack_and_confirm }
 
-  scenario 'reduced points for player 2 after being attacked' do
-    sign_in_and_play
-    attack_and_confirm
-    expect(page).not_to have_content 'Alan: 60 HP'
-    expect(page).to have_content 'Alan: 50 HP'
-  end
+    scenario 'reduced points for player 2 after being attacked' do
+      expect(page).not_to have_content 'Alan: 60 HP'
+      expect(page).to have_content 'Alan: 50 HP'
+    end
 
-  scenario 'reduced points for player 1 after being attacked' do
-    sign_in_and_play
-    2.times { attack_and_confirm }
-    expect(page).not_to have_content 'Yuliya: 60 HP'
-    expect(page).to have_content 'Yuliya: 50 HP'
+    scenario 'reduced points for player 1 after being attacked' do
+      attack_and_confirm
+      expect(page).not_to have_content 'Yuliya: 60 HP'
+      expect(page).to have_content 'Yuliya: 50 HP'
+    end
   end
 end
