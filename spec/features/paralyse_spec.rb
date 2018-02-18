922e0078595512f89939d0c01024df8e183d73f0
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-02-18 22:54:41 +0000

    Correct tests.

--- spec/features/paralyse_spec.rb
+++ spec/features/paralyse_spec.rb
@@ -1,4 +1,7 @@
 feature 'Player can make his opponent lose the chance to attack', js: true do
+  before do
+    allow_any_instance_of(Array).to receive(:sample).and_return true
+  end
   scenario 'Player 1 manages to paralyse his opponent' do
     sign_in_and_play
     click_button 'Paralyse'
@@ -6,10 +9,6 @@ feature 'Player can make his opponent lose the chance to attack', js: true do
   end
 #
   scenario 'Player 2 manages to paralyse his opponent' do
-    # player1 = instance_double('Player')
-    # array = Array.new
-    # allow(array).to receive(:sample).and_return true
-    allow_any_instance_of(Player).to receive(:paralysed?).and_return true
     sign_in_and_play
     click_button 'Attack'
     click_button 'Paralyse'
@@ -17,9 +16,8 @@ feature 'Player can make his opponent lose the chance to attack', js: true do
   end
 
   scenario "Player 1 doesn't succeed to paralyse his opponent" do
-    # player = instance_double('Player')
     sign_in_and_play
-    # allow(player).to receive(:paralysed?).and_return false
+    allow_any_instance_of(Array).to receive(:sample).and_return false
     click_button 'Paralyse'
     expect(page).to have_content 'You missed your chance to paralyse Alan. 😛😛😛'
   end
